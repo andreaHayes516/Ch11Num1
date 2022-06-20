8e1f37842450fded367cee85c4179eb12f42208f
Andrea Hayes <amock516@gmail.com>
2022-06-20 13:58:44 -0700

    Add project files.

---
+++ Ch11Num1/Ch11Num1MovieData.cpp
@@ -0,0 +1,55 @@
+/************************************************
+** Author: Andrea Hayes
+** Date: June 7, 2020
+** Purpose: Store Movie info in a structure
+** Input: Movie Data
+** Processing: Structure
+** Output: Movie info
+*************************************************/
+#include <iostream>
+#include <iomanip>
+#include <string>
+using namespace std;
+// Declare Sturcture
+struct MovieData
+{
+    string title;
+    string director;
+    int year;
+    int runningTime;
+};
+void showMovie(MovieData);
+int main() {
+    // Declare Variables
+    const int SIZE = 2;
+    MovieData movies[SIZE];
+    // Get user input
+    for (int i = 0; i < SIZE; i++) {
+        cout << "Enter Movie Title: ";
+        getline(cin, movies[i].title);
+        cout << "Director: ";
+        getline(cin, movies[i].director);
+        cout << "Year: ";
+        cin >> movies[i].year;
+        cout << "Run Time in Minutes: ";
+        cin >> movies[i].runningTime;
+        string nothing;
+        getline(cin, nothing);
+        cout << endl;
+    }
+    for (int i = 0; i < SIZE; i++) {
+        showMovie(movies[i]);
+    }
+    
+    return 0;
+}
+// Function to Display data
+void showMovie(MovieData movies)
+{
+        cout << fixed << showpoint << setprecision(2);
+        cout << "Title: " << movies.title << endl;
+        cout << "Director: " << movies.director << endl;
+        cout << "Year: " << movies.year << endl;
+        cout << "Run Time: " << movies.runningTime << endl;
+        cout << endl;
+}
